2b1d18191824ff755dbf1849d840acf0bbc72f4e
developeraravinth05 <abiaravinth05@gmail.com>
2024-07-20 10:36:09 +0530

    code added for array and array list

--- JAVA/src/Arrays/SwapAndReverse.java
+++ JAVA/src/Arrays/SwapAndReverse.java
@@ -2,37 +2,82 @@ package Arrays;
 
 import java.util.Arrays;
 
+import java.util.Arrays;
+
 public class SwapAndReverse {
     public static void main(String[] args) {
-        int[] arr={1,2,3,4,5,6};
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int[][] arrMulti = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
 
-        swap(arr,0,1);
+        // Swap elements in 1D array
+        swap(arr, 0, 1);
         System.out.println(Arrays.toString(arr));
+
+        // Reverse elements in 1D array
         reverse(arr);
         System.out.println(Arrays.toString(arr));
 
+        // Swap rows in 2D array
+        swap(arrMulti, 0, 1);
+        System.out.println("Swapped rows in 2D array: " + Arrays.deepToString(arrMulti));
 
+        // Reverse rows in 2D array
+        reverse(arrMulti);
+        System.out.println("Reversed rows in 2D array: " + Arrays.deepToString(arrMulti));
+
+        // Transpose the 2D array
+        transpose(arrMulti);
+        System.out.println("Transposed 2D array: " + Arrays.deepToString(arrMulti));
     }
 
+    // Reverse a 1D array
     private static void reverse(int[] arr) {
-  int startIndex=0;
-  int endIndex=arr.length-1;
-  while (startIndex<endIndex){
-
-      swap(arr,startIndex,endIndex);
-      startIndex++;
-      endIndex--;
-  }
-
+        int startIndex = 0;
+        int endIndex = arr.length - 1;
+        while (startIndex < endIndex) {
+            swap(arr, startIndex, endIndex);
+            startIndex++;
+            endIndex--;
+        }
     }
 
-    //first we store index1 in var1 and we are saying index1 = index2 which will be the value in
-    //index 1
+    // Swap elements in a 1D array
     private static void swap(int[] arr, int index1, int index2) {
-    int temp=arr[index1];
-    arr[index1]=arr[index2];
-    arr[index2]=temp;
-
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
     }
 
+    // Swap rows in a 2D array
+    private static void swap(int[][] arr, int index1, int index2) {
+        int[] temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+
+    // Reverse rows in a 2D array
+    private static void reverse(int[][] arr) {
+        int startIndex = 0;
+        int endIndex = arr.length - 1;
+        while (startIndex < endIndex) {
+            swap(arr, startIndex, endIndex);
+            startIndex++;
+            endIndex--;
+        }
+    }
+
+    // Transpose a 2D array
+    private static void transpose(int[][] arr) {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = row + 1; col < arr[row].length; col++) {
+                int temp = arr[row][col];
+                arr[row][col] = arr[col][row];
+                arr[col][row] = temp;
+            }
+        }
+    }
 }
--- JAVA/src/Arrays/transpose.java
+++ JAVA/src/Arrays/transpose.java
@@ -1,28 +1,64 @@
 package Arrays;
 
+import java.lang.reflect.AnnotatedArrayType;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class transpose {
     public static void main(String[] args) {
-        int[][] arr={
-                {1,2,3},
-                {4,5,6},
-                {7,8,9}
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
         };
+
+//way 1
         int[][] transposedArr = new int[3][3];
-        for (int row=0;row< 2;row++){
-        for (int col=0;col<arr[row].length;col++){
-            transposedArr[row][col]=arr[col][row];
-            System.out.print(transposedArr[row][col]);
+        for (int row=0;row< arr.length;row++){
+            for (int col=0;col<arr[row].length;col++){
+                transposedArr[row][col]=arr[col][row];
+                System.out.print(transposedArr[row][col]+ " ");
+            }
+            System.out.println();
         }
+// Transpose the matrix
+        for (int[] array:transposedArr){
+
+            System.out.println(Arrays.toString(array));
+        }
+
+
+
+       //way 2
+
+
+        // Transpose the matrix in place
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = row + 1; col < arr[row].length; col++) {
+                int temp = arr[row][col];
+                arr[row][col] = arr[col][row];
+                arr[col][row] = temp;
+            }
+        }
+
+         //using deep string
+        System.out.println(Arrays.deepToString(arr));
+
+        // Print the transposed matrix using for each
+        System.out.println("Transposed matrix:");
+        for (int[] row : arr) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
             System.out.println();
         }
 
-for (int[] array:transposedArr){
-
-    System.out.println(Arrays.toString(array));
-}
+        //Using Arrays
+        // Print the transposed matrix using Arrays.toString
+        System.out.println("Transposed matrix:");
+        for (int[] row : arr) {
+            System.out.println(Arrays.toString(row));
+        }
 
     }
 }
